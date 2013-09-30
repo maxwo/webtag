492608aeafabec8c5b5524ad6f0c5bc7cf1a556a
MaxWo <maxime.wojtczak@yahoo.fr>
2013-09-30 08:06:56 +0200

    Inode manager Error handling

---
+++ managers/inode.js
@@ -0,0 +1,75 @@
+var indexer = require('../indexer');
+var tools = require('../tools');
+
+exports.inode = function() {
+
+    return {
+       id: '',
+       filename: '',
+       'content-type': '',
+       size: 0,
+       tags: [],
+       metadata: [],
+       owner: '',
+       groups: []
+   };
+
+};
+
+exports.inodeHandler = function(request, response, next) {
+
+    var id = request.params.id;
+    var search = indexer.beginInodeGet(id);
+
+    search.on('found', function(inode) {
+        console.log('found');
+        request.inode = inode;
+        next();
+    });
+
+    search.on('notFound', function() {
+        console.log('not found');
+        response.writeHead(404);
+        response.end('This inode doesn\'t exists.');
+        tools.errorHandler(response)({notFound: true, type: 'inode', id: id});
+    });
+
+    search.on('error', tools.errorHandler(response));
+
+    search.process();
+
+};
+
+exports.tagsHandler = function(request, response, next)
+{
+    var tags = request.path.substr(6).split('/');
+    request.tags = [];
+
+    for ( var i=0 ; i<tags.length ; i++ ) {
+        if ( tags[i] ) {
+            request.tags.push(tags[i]);
+        }
+    }
+
+    next();
+};
+
+exports.checkSentInode = function(sentInode, foundCallback, errorCallback) {
+
+    var search = indexer.beginInodeGet(sentInode.id);
+
+    search.on('found', function(inode) {
+        foundCallback(inode);
+    });
+
+    search.on('notFound', function() {
+        errorCallback({notFound: true, type: 'inode', inode: sentInode, id: sentInode.id});
+    });
+
+    search.on('error', function() {
+        errorCallback({error: true, type: 'inode', inode: sentInode, id: sentInode.id});
+    });
+
+    search.process();
+
+};
---
+++ tools.js
@@ -0,0 +1,58 @@
+/**
+ * Error object for generic errors
+ */
+exports.Error = function(context) {
+
+    context = context ? context : 'An error occured.';
+    context = typeof context==='string' ? {message: context} : context;
+
+    this.context = context;
+};
+exports.Error.prototype = new Error();
+
+/**
+ * Error object for inode errors
+ */
+exports.InodeError = function(inode, context) {
+
+    context.type = 'inode';
+    context.inode = inode;
+    context.id = inode.id;
+
+    exports.Error.apply(this, [context]);
+}
+exports.InodeError.prototype = new exports.Error();
+
+
+exports.DataError = function(stream, context) {
+
+    context.type = 'data';
+    context.stream = stream;
+
+    exports.Error.apply(this, [context]);
+
+};
+
+
+
+
+exports.errorHandler = function(response) {
+
+    return function(error) {
+
+        console.log('error while processing a request');
+
+        if ( error.context.notFound ) {
+            response.writeHead(404);
+        } else if ( error.context.badRequest ) {
+            response.writeHead(400);
+        } else {
+            response.writeHead(500);
+        }
+
+        response.write(JSON.stringify(error, null, 4));
+        response.end();
+
+    };
+
+};
