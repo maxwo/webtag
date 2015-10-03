4ad73b2fc057565f46fd16a475cd981c5a04177c
Maxime Wojtczak <max@aki.home>
2015-10-03 19:31:47 +0200

    Upload correction.

--- server.js
+++ server.js
@@ -1,45 +1,46 @@
 var express = require('express'),
-    http = require('http'),
-    events = require('events');
+	http = require('http'),
+	path = require('path');
 
-var userManager = require('./lib/managers/user');
+// var userManager = require('./lib/managers/user');
 var notification = require('./lib/notification');
 var tools = require('./lib/tools');
 var config = require('./lib/config');
-
 
 var dataEndPoint = require('./lib/data');
 var inodeEndPoint = require('./lib/inode');
 var userEndPoint = require('./lib/user');
 var tagsEndPoint = require('./lib/tags');
 
-
-
-
 var app = express();
 var server = http.createServer(app);
 
-app.use(express.basicAuth( userManager.authenticate ));
+// app.use(express.basicAuth( userManager.authenticate ));
+app.use(function (request, response, next) {
+	console.log('Passage dans le middlerware.');
+	request.user = {
+		login: 'max',
+		groups: []
+	};
+	return next();
+});
 app.use(express.methodOverride());
 app.use(express.logger());
 
-app.get('/*', express.static(__dirname + '/static'));
+app.get('/*', express.static(path.join(__dirname, 'static')));
 
 app.use('/api/user/', express.bodyParser());
 app.use('/api/inode/', express.bodyParser());
 app.use('/api/tags/', express.bodyParser());
-app.use(function(error, request, response, next) {
-    if (!error) {
-        return next();
-    }
-    tools.errorHandler(response)({
-        error:true,
-        source: error
-    });
+app.use(function (error, request, response, next) {
+	if (!error) {
+		return next();
+	}
+	tools.errorHandler(response)({
+		error: true,
+		source: error
+	});
 });
-
-
-
 
 app.use(dataEndPoint);
 app.use(inodeEndPoint);
@@ -51,4 +52,3 @@ notification.initNotification(app, server);
 tools.logger.info('Preparing to listen on %s:%d', config.get('httpHost'), config.get('httpPort'));
 server.listen(config.get('httpPort'), config.get('httpHost'));
 tools.logger.info('Listening on %s:%d', config.get('httpHost'), config.get('httpPort'));
-
