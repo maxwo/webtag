0797d49178caf33cbb987a72f21a555daed1d899
Maxime Wojtczak <max@macbook-pro-de-maxime.home>
2016-05-27 10:05:23 +0200

    feat(DevTools) : Add user who uploads.

--- dev/bin/sendFile.sh
+++ dev/bin/sendFile.sh
@@ -2,11 +2,13 @@
 
 if [ -z $1 ]
 then
-    echo "Usage: $0 FILE [TAG1 TAG2 ...]"
+    echo "Usage: $0 USERNAME FILE [TAG1 TAG2 ...]"
 fi
 
-FILENAME=$1
+USERNAME=$1
+shift;
 
+FILENAME=$1
 shift;
 
 TAGS=$@;
@@ -14,4 +16,4 @@ TAGS=$@;
 function join { local IFS="$1"; shift; echo "$*"; }
 TAGS=`join , ${TAGS[@]}`
 
-curl --request POST --form tags=$TAGS --form "file=@$FILENAME" --insecure --cert ../certificates/clients/max.p12:max https://localhost:4433/api/data/
+curl --request POST --form tags=$TAGS --form "file=@$FILENAME" --insecure --cert ../certificates/clients/$USERNAME.p12:test https://localhost:4433/api/data/
